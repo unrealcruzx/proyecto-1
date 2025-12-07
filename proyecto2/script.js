5960b061caf99a9dcc87c1f1f00d5c22418094e2
Santiago <cruzxzx@icloud.com>
2025-12-07 16:36:02 -0500

    cambios en script

--- proyecto2/script.js
+++ proyecto2/script.js
@@ -4,17 +4,20 @@ input.addEventListener("keyPress", function(e)  {
 
 });
 
-function addTask() {
-    const taskText = input.ariaValueMax.trim();
+ function addTask() {
+    const input = document.getElementById("taskInput")
+    const taskInput = input.ariaValueMax.trim();
+
     if (taskText === "") return;
 
     const li = document.createElement("li");
-    li.innerHTML = taskText + '<button onclick="removeTask(this)">X</button>';
-    li.addEventListener("click", () => toggleComplete(li));
+    li.innerHTML = 
+    taskText +
+    '<button onclick="removeTask(this)">X</button>';
 
     document.getElementById("taskList").appendChild(li);
     input.value = "";
-}
+ }
 
 function removeTask(button) {
     button.parentElement.remove();
