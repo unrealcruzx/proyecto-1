55df4f0abd667d00f4ef01dcce1682055814c172
Santiago <cruzxzx@icloud.com>
2025-12-08 15:31:09 -0500

    reorganizo estructura de proyectos

---
+++ proyecto2/script.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("taskInput");
+input.addEventListener("keyPress", function(e)  {
+    if (e.key === "Enter") addTask();
+
+});
+
+ function addTask() {
+    const input = document.getElementById("taskInput")
+    const taskInput = input.ariaValueMax.trim();
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.innerHTML = 
+    taskText +
+    '<button onclick="removeTask(this)">X</button>';
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+ }
+
+function removeTask(button) {
+    button.parentElement.remove();
+}
+
+function toggleComplete(li) {
+    li.classList.toggle("completed");
+}
+
+function cleanTask() {
+    document.getElementById("taskList").innerHTML = "";
+}
+
